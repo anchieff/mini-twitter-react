966bc1c70f60aa8cb575d68ad5ecc9aa6d150087
Anna <averbah-ann@mail.ru>
2020-12-24 11:18:05 +0400

    Finish app

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,7 +3,7 @@ import PostListItem from '../post-list-item/post-list-item';
 import {ListGroup} from 'reactstrap';
 import './post-list.css';
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
   const elements = posts.map(item => {
     const {id, ...itemProps} = item;
     return (
@@ -11,6 +11,8 @@ const PostList = ({posts, onDelete}) => {
         <PostListItem
         {...itemProps}
         onDelete={() => onDelete(id)}
+        onToggleImportant={() => onToggleImportant(id)}
+        onToggleLiked={() => onToggleLiked(id)}
       />
     </li>
     )
